e3afb4073c1524a4c11a18eae09937441312337e
Stephen Touset <stephen@touset.org>
2008-12-15 16:18:25 -0500

    Conditionally define shuffle, in case of Ruby 1.8.7

--- lib/core_ext/array/shuffle.rb
+++ lib/core_ext/array/shuffle.rb
@@ -1,16 +1,20 @@
 class Array
-  #
-  # Randomizes the contents of the Array, replacing the existing Array with
-  # the shuffled one.
-  #
-  def shuffle!
-    replace(shuffle)
+  unless respond_to?(:shuffle!)
+    #
+    # Randomizes the contents of the Array, replacing the existing Array with
+    # the shuffled one.
+    #
+    def shuffle!
+      replace(shuffle)
+    end
   end
   
-  #
-  # Returns a new Array with the shuffled contents of the original.
-  #
-  def shuffle
-    sort_by { rand }
+  unless respond_to?(:shuffle)
+    #
+    # Returns a new Array with the shuffled contents of the original.
+    #
+    def shuffle
+      sort_by { rand }
+    end
   end
 end
