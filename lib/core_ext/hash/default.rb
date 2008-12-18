160deada789e1084bc8f593d7035ba115a447736
Stephen Touset <stephen@touset.org>
2008-12-18 16:06:48 -0500

    Documentation

--- lib/core_ext/hash/default.rb
+++ lib/core_ext/hash/default.rb
@@ -1,4 +1,9 @@
 class Hash
+  #
+  # Creates a new Hash where the default value of each key is the value
+  # returned by the block passed. Keys are automatically assigned their
+  # default values when used.
+  #
   def self.default(&block)
     Hash.new {|h,k| h[k] = block.call(k) }
   end
